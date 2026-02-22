52add71d9c44ed05b1c2c5a0cebda44c0662ed5e
YuvrajMangutkar <mangutkaryuvraj@gmail.com>
2026-02-22 10:37:07 +0530

    timeline Rescheduling

--- frontend/projects/views.py
+++ frontend/projects/views.py
@@ -3,8 +3,9 @@ from .models import Project, Task, Progress, AIInsight
 from .planner import generate_tasks
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
-from datetime import datetime
+from datetime import datetime,timedelta
 from .critic import run_critic
+
 
 
 def dashboard(request):
@@ -20,31 +21,38 @@ def project_detail(request, project_id):
 def create_project(request):
     if request.method=="POST":
         goal=request.POST.get("goal")
-        total_days=request.POST.get("total_days")
+        total_days=int(request.POST.get("total_days"))
 
         #create project first
         project=Project.objects.create(
             user=request.user,
             goal=goal,
-            total_days=int(total_days)
+            total_days=total_days
 
         )
         try:
             tasks=generate_tasks(goal,total_days)
-
+            current_date=project.start_date
             for t in tasks:
+                estimated=int(t.get("estimated_days",1))
+                due_date=current_date+timedelta(days=estimated)
                 Task.objects.create(
                     project=project,
                     title=t.get('title',"Untitled Task"),
                     description=t.get("description",""),
                     priority=t.get("priority","medium"),
-                    estimated_days=t.get("estimated_days",1)
+                    estimated_days=t.get("estimated_days",1),
+                    due_date=due_date
                 )
+                current_date=due_date
         except Exception as e:
             print("Planner error:",e)
         
         return redirect("dashboard")
     return render(request,"projects/create_project.html")
+   
+
+
 
 def complete_task(request,task_id):
     task=Task.objects.get(id=task_id)
@@ -61,10 +69,22 @@ def complete_task(request,task_id):
         if actual_days>task.estimated_days:
             delay=actual_days-task.estimated_days
 
+            #setting an timeline for the future pending tasks
+            future_tasks=Task.objects.filter(
+                project=task.project,
+                status="pending",
+                due_date__gt=task.due_date  
+            ).order_by("due_date")
+
+            for ft in future_tasks:
+                ft.due_date=ft.due_date+timedelta(days=delay)
+                ft.save()
+
             AIInsight.objects.create(
                 project=task.project,
                 agent_type="scheduler",
-                message=f"Task {task.title} was delayed by {delay} days.Consider adjusting future task timelines."
+                message=f"Task {task.title} was delayed by {delay} days."
+                f"{future_tasks.count()} future_tasks were rescheduled."
             )
         run_critic(task.project)
         
